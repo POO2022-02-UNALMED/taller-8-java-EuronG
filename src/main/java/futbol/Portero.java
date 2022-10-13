24de3e23149835af7ea952907441706e9c90dac4
EuronG <jugiraldoja@unal.edu.co>
2022-10-13 18:19:56 -0500

    0.3

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -6,7 +6,7 @@ public class Portero extends Futbolista {
     public byte dorsal;
 
     public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
-        super(nombre, edad, "futbol.Portero");
+        super(nombre, edad, "Portero");
         this.golesRecibidos = golesRecibidos;
         this.dorsal = dorsal;
     }
